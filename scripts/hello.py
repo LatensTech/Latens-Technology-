706a877f4b680259c4a081884ff5bc686287e54c
LatensTech <petermainaah375@gmail.com>
2025-05-16 14:12:56 +0300

    Initial Commit of all tools and bots

---
+++ scripts/hello.py
@@ -0,0 +1,3 @@
+print("Welcome to Termux Python scripting!")
+name = input("What's your name? ")
+print(f"Hello, {name}. You're now coding like a pro!")
---
+++ scripts/morning.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+termux-notification \
+  --title "Morning Reminder" \
+  --content "LEVEL UP DAILY M" \
+  --priority high \
+  --sound
